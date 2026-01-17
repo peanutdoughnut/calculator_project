33fdbd9aa407fb4faa531f79363603d145b01043
peanutdoughnut <cjkwanthe@gmail.com>
2026-01-17 12:20:39 +0200

    added functions for calculating operations added a function to choose which operation to carry out based on the operator selected

--- calc.js
+++ calc.js
@@ -3,3 +3,33 @@
 addition = (num1, num2) => {
     return Number(num1) + Number(num2);
 }
+
+//function for subtracting two numbers
+subtraction = (num1,num2) => {
+    return Number(num1) - Number(num2);
+}
+
+//function for multiplying two numbers
+multiply = (num1,num2) => {
+    return Number(num1) * Number(num2);
+}
+
+//function for dividing two numbers 
+divide = (num1,num2) => {
+    return Number(num1) / Number(num2);
+}
+
+//function to select calculation needed based on operator selected
+operator = (op, num1, num2) => {
+    if (op === '+') {
+        return addition(num1, num2);
+    } else if (op === '-') {
+        return subtraction(num1, num2);
+    } else if (op === '*') {
+        return multiply(num1, num2);
+    } else if (op === '/') {
+        return divide(num1, num2);
+    } else {
+        return 'Invalid operator';
+    }
+}
